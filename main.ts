752a37949bef5c72d992991fd015bba616b4002c
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 17:37:20 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
-basic.forever(function on_forever() {
-    if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 90)
-    } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinRight, 75)
-    } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.Black) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinLeft, 75)
+basic.forever(function () {
+    if (mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.LeftState, mbit_&#23567;&#36554;&#39006;.enLineState.White) && mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.RightState, mbit_&#23567;&#36554;&#39006;.enLineState.White)) {
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_Run, 90)
+    } else if (mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.LeftState, mbit_&#23567;&#36554;&#39006;.enLineState.White) && mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.RightState, mbit_&#23567;&#36554;&#39006;.enLineState.Black)) {
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_SpinRight, 75)
+    } else if (mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.LeftState, mbit_&#23567;&#36554;&#39006;.enLineState.Black) && mbit_小車類.Line_Sensor(mbit_&#23567;&#36554;&#39006;.enPos.RightState, mbit_&#23567;&#36554;&#39006;.enLineState.White)) {
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_SpinLeft, 75)
     } else {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinRight, 50)
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 20)
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinLeft, 50)
-        mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Stop)
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_SpinRight, 50)
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_Run, 20)
+        mbit_小車類.CarCtrlSpeed(mbit_&#23567;&#36554;&#39006;.CarState.Car_SpinLeft, 50)
+        mbit_小車類.CarCtrl(mbit_&#23567;&#36554;&#39006;.CarState.Car_Stop)
     }
-    
 })
