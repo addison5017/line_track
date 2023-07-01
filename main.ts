69e8403ca2edfaa9e3c8f029fcce993a837d6b34
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 16:38:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 basic.forever(function () {
     if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 100)
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 120)
     } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinRight, 50)
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinRight, 80)
     } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.Black) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinLeft, 50)
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinLeft, 80)
     } else {
         mbit_小車類.CarCtrl(mbit_小車類.CarState.Car_Stop)
     }
