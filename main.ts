98297ce8bbd6ae036025f77c8f286b20e59e7f37
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 16:26:23 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 100)
+    } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Right, 50)
+    } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.Black) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Left, 50)
+    } else {
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 0)
+    }
 })
