4a4f21c6ce5c041322c44744189139f59b1e3128
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 17:20:12 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
-        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 120)
+        mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_Run, 100)
     } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.White) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.Black)) {
         mbit_小車類.CarCtrlSpeed(mbit_小車類.CarState.Car_SpinRight, 75)
     } else if (mbit_小車類.Line_Sensor(mbit_小車類.enPos.LeftState, mbit_小車類.enLineState.Black) && mbit_小車類.Line_Sensor(mbit_小車類.enPos.RightState, mbit_小車類.enLineState.White)) {
